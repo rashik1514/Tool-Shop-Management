3c887cf64073a345d6f61035980c63f2520f790f
Christina <christinalu42@gmail.com>
2019-04-02 10:25:39 -0600

    User GUI general format

--- src/Client/View/UserGUI.java
+++ src/Client/View/UserGUI.java
@@ -1,4 +1,37 @@
 package Client.View;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
 public class UserGUI {
+
+    private JPanel panel1;
+    private JScrollBar scrollBar1;
+    private JButton searchButton;
+    private JTextField searchBarTextField;
+    private JButton showToolsButton;
+    private JButton exitButton;
+    private JTextArea textArea1;
+
+    public UserGUI() {
+        //TODO: insert database stuff
+    }
+
+    public void addListeners(){
+        showToolsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //TODO: database.display() ??
+            }
+        });
+        searchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s  = searchBarTextField.getText();
+                //TODO: database.search(s) ??
+            }
+        });
+
+    }
+
 }
