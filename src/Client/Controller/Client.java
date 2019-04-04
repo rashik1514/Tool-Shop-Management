17158633932976e52e930f5142756850be72e8d1
Christina <christinalu42@gmail.com>
2019-04-04 17:21:33 -0600

    server and client working!

--- src/Client/Controller/Client.java
+++ src/Client/Controller/Client.java
@@ -70,12 +70,13 @@ public class Client {
         socketOut.println("DISPLAY TOOLS");
         String response = socketIn.readLine();
         StringBuilder data = new StringBuilder();
-        while (!response.equals("DISPLAY TOOLS")) {
-            data.append(response);
-            data.append("\n");
-            response = socketIn.readLine();
-        }
-        return data.toString();
+//        while (!response.equals("DISPLAY TOOLS")) {
+//            data.append(response);
+//            data.append("\n");
+//            response = socketIn.readLine();
+//        }
+//        return data.toString();
+        return response;
     }
 
 }
