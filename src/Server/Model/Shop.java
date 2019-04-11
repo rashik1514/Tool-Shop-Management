79e7ae9b4e64e7d1dc7a1b47b9ad15848e27daee
Layla Arab <layla.e.arab@gmail.com>
2019-04-11 14:10:03 -0600

    numb

--- src/Server/Model/Shop.java
+++ src/Server/Model/Shop.java
@@ -1,14 +1,32 @@
 package Server.Model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 
-public class Shop extends Database {
+public class Shop extends Database implements Runnable {
 
     private Inventory theInventory;
+    private BufferedReader socketIn;
+    private PrintWriter socketOut;
+    private Socket socket;
 
-    public Shop(Inventory inventory) {
+    public Shop(Inventory inventory, Socket s) {
         theInventory = inventory;
+        socket = s;
+        try {
+            socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            socketOut = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 
     public String decreaseItem(String name) {
         if (theInventory.manageItem(name) == null)
@@ -40,6 +58,59 @@ public class Shop extends Database {
         return theInventory.printOrder();
     }
 
+    public void run() {
+        String in = "";
+        while (true) {
+            try {
+                in = socketIn.readLine();
+
+                if (in.equals("DISPLAY")) {
+                    String out = loadItemsTable();
+                    socketOut.println(out);
+                    socketOut.println("END");
+
+                } else if (in.equals("SEARCHID")) {
+                    int id = Integer.parseInt(socketIn.readLine());
+                    String out = searchByItemId(id);
+                    socketOut.println(out);
+                    socketOut.println("END");
+
+
+                } else if (in.equals("SEARCHNAME")) {
+                    String name = socketIn.readLine();
+                    String out = searchByItemName(name);
+                    socketOut.println(out);
+                    socketOut.println("END");
+
+
+                } else if (in.equals("DECREASEID")) {
+                    int id = Integer.parseInt(socketIn.readLine());
+                    int amount = Integer.parseInt(socketIn.readLine());
+                    String out = changeItemQuantity(amount, id);
+                    socketOut.println(out);
+                    socketOut.println("END");
+
+
+                } else if (in.equals("DECREASENAME")) {
+                    String name = socketIn.readLine();
+                    int amount = Integer.parseInt(socketIn.readLine());
+                    String out = changeItemQuantity(amount, name);
+                    socketOut.println(out);
+                    socketOut.println("END");
+
+
+                } else if (in.equals("QUIT")) {
+                    break;
+                }
+            } catch (SocketException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+        }
+    }
 
 }
 
